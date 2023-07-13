a19c726418f3af3066d328000eb6493b34093851
therohit25 <rohitghadage40@gmail.com>
2023-07-13 21:19:04 +0530

    Updates

---
+++ precache-manifest.0aa4bd53371cbc56cf1ff2856f743168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af001a94c466451608e9",
+    "url": "/ReactTicketsApp/static/js/main.af001a94.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/ReactTicketsApp/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "94b7143cd93f0ccf7ba8",
+    "url": "/ReactTicketsApp/static/css/2.8fc61f8b.chunk.css"
+  },
+  {
+    "revision": "94b7143cd93f0ccf7ba8",
+    "url": "/ReactTicketsApp/static/js/2.94b7143c.chunk.js"
+  },
+  {
+    "revision": "1bb5e0614758b0528195",
+    "url": "/ReactTicketsApp/static/js/runtime~main.1bb5e061.js"
+  },
+  {
+    "revision": "a178debd7e214369529b9a3e8759890d",
+    "url": "/ReactTicketsApp/static/media/ticket.a178debd.jpg"
+  },
+  {
+    "revision": "b3fda78272bd76a01a401e79aca0a127",
+    "url": "/ReactTicketsApp/index.html"
+  }
+];
